3d7bd36aaa99b0a11f256df12955aa36aab04f15
Pedmuncif <60979645+Pedmuncif@users.noreply.github.com>
2021-01-18 16:28:48 +0100

    Arreglo servicio comentarioHotel

--- src/main/java/org/springframework/samples/petclinic/service/ComentarioHotelService.java
+++ src/main/java/org/springframework/samples/petclinic/service/ComentarioHotelService.java
@@ -38,7 +38,23 @@ public class ComentarioHotelService {
 	public void savec(int id, ComentarioHotel comentario) throws DataAccessException{
 		Hotel h = hotelRepository.findById(id);
         h.getComentarios().add(comentario);
+        List<ComentarioHotel> lscomen = new ArrayList<ComentarioHotel>();
+       
         comentario.setHotel(h);
+        lscomen.addAll(h.getComentarios());
+        if(lscomen.size() >= 5) {
+            Double media = 0.;
+            for(int i = 0; i<lscomen.size();i++){
+                Integer puntuacion = lscomen.get(i).getPuntuacion();
+                media = (media + puntuacion);
+            }
+            media = media/lscomen.size();
+            if(media <= 2) {
+            	hotelRepository.findById(id).setValido(false);
+            }else {
+            	hotelRepository.findById(id).setValido(true);
+            }
+        }
         comentarioRepository.save(comentario);
 	}
 
