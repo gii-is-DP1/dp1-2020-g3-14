d4d47c0e12d2d0438912b3a724ad6cd4317a7ddc
Jose Francisco <josregmej@alum.us.es>
2021-02-05 16:52:15 +0100

    Pruebas del service de Habitacion

--- src/main/java/org/springframework/samples/petclinic/service/HabitacionService.java
+++ src/main/java/org/springframework/samples/petclinic/service/HabitacionService.java
@@ -3,6 +3,7 @@ package org.springframework.samples.petclinic.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.samples.petclinic.model.Habitacion;
+import org.springframework.samples.petclinic.model.Hotel;
 import org.springframework.samples.petclinic.repository.HabitacionRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,5 +22,15 @@ public class HabitacionService {
 	@Transactional(readOnly = true)
 	public Habitacion findHabitacionById(int id) throws DataAccessException {
 		return habitacionRepository.findByNhabitacionLike(id);
+	}
+	
+	@Transactional
+	public void saveHabitacion(Habitacion habitacion) throws DataAccessException {
+		habitacionRepository.save(habitacion);                
+	}
+	
+	@Transactional
+	public void deleteHabitacion(Habitacion habitacion) throws DataAccessException {
+		habitacionRepository.delete(habitacion);            
 	}
 }
